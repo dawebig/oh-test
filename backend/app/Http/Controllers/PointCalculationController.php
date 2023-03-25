aa01933786e0994d9ab397f28dbd0fdcccb2b3f1
dawebig <dawebig@gmail.com>
2023-03-25 19:02:23 +0100

    feat: Add calculate controller and route

---
+++ backend/app/Http/Controllers/PointCalculationController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Routing\Controller as BaseController;
+
+class PointCalculationController extends BaseController
+{
+    public function calculate() {
+        dd('Test');
+    }
+}
+
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Enums\RouteNames;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PointCalculationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/calculate', [PointCalculationController::class, 'calculate'])->name(RouteNames::CALCULATE);
