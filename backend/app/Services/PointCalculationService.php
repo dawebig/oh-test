c628b8037570036c45942f2fd3e76cd26a928466
dawebig <dawebig@gmail.com>
2023-03-26 07:26:51 +0200

    feat: Add point calculation

--- backend/app/Services/PointCalculationService.php
+++ backend/app/Services/PointCalculationService.php
@@ -11,32 +11,113 @@ use Symfony\Component\HttpFoundation\Response;
 class PointCalculationService
 {
     private Collection $results;
+    private Collection $extras;
 
     public function __construct()
     {
         $this->results = collect();
+        $this->extras  = collect();
     }
 
     /* @param Request $request
     * @return JsonResponse
     */
-    public function calculatePoint(Request $request)  {
+    public function calculatePoint(Request $request): JsonResponse
+    {
+        $this->setResultToCollection($request);
+        $this->setExtraToCollection($request);
+
         list($isValid, $validationMessage) = $this->validate($request);
 
         if(!$isValid) {
             return response()->json($validationMessage, Response::HTTP_BAD_REQUEST);
         }
 
-        return response()->json(null, Response::HTTP_OK);
+        $defaultPoint = $this->calculateDefaultPoint($request);
+        $extraPoint = $this->calculateHighLevelPoints()+$this->calculateLanguagePoints();
+
+        return response()->json([
+            'point' => $defaultPoint + ($extraPoint > 100 ? 100 : $extraPoint)
+        ], Response::HTTP_OK);
+    }
+
+    /**
+     * @param Request $request
+     * @return float|int
+     */
+    private function calculateDefaultPoint(Request $request) {
+
+        $university = $this->getUniversityInformation($request);
+
+        $requiredPoint = 0;
+        foreach ($this->results as $item) {
+            if ( in_array($item['name'], [$university['subjects']['required']['name']]) ) {
+                if (isset($university['subjects']['required']['level'])) {
+                    if ($university['subjects']['required']['level'] == $item['type']) {
+                        $requiredPoint = $item['result'];
+                    }
+                }
+                else {
+                    $requiredPoint = $item['result'];
+                }
+            }
+        }
+
+        $optionalPoint = 0;
+        foreach ($this->results as $item) {
+            if ( in_array($item['name'], $university['subjects']['optionals']) ) {
+                if ($optionalPoint < $item['result'] ) {
+                    $optionalPoint = $item['result'];
+                }
+            }
+        }
+
+
+        return ($requiredPoint+$optionalPoint)*2;
+    }
+
+    /**
+     * @return number
+     */
+    private function calculateHighLevelPoints(): float|int
+    {
+        $filtered = $this->results->filter(function ($item) {
+            return $item['type'] == 'emelt';
+        });
+
+        return $filtered->count()*50;
+    }
+
+    /**
+     * @return number
+     */
+    private function calculateLanguagePoints(): int
+    {
+        $languages = [];
+        foreach ($this->extras as $item) {
+            switch ($item['level']) {
+                case 'C1':
+                    $point = 40;
+                    break;
+                case 'B2':
+                    $point = 28;
+                    break;
+                default:
+                    $point = 0;
+            }
+            if ( !isset($languages[$item['language']]) || $languages[$item['language']] < $point) {
+                $languages[$item['language']] = $point;
+            }
+        }
+        return array_sum($languages);
     }
 
     /**
      * @param Request $request
      * @return array
      */
-    private function validate(Request $request) {
-        $this->setResultToCollection($request);
-
+    private function validate(Request $request): array
+    {
         $foundRequiredSubjects = $this->validateAllContainSubject(['magyar nyelv és irodalom' ,'történelem', 'matematika']);
         if ( !$foundRequiredSubjects ) {
             return [false, 'A pontszámítás nem lehetséges. (Kötelező érettségi tantárgy hiánya)'];
@@ -72,7 +153,6 @@ class PointCalculationService
             if ($item['result'] < 20 ) {
                 $isUpper = false;
             }
-
         }
         return $isUpper;
     }
@@ -130,7 +210,7 @@ class PointCalculationService
      * @param Request $request
      * @return array
      */
-    private function getUniversityInformation(Request $request)
+    private function getUniversityInformation(Request $request): array
     {
         $university = $request->get('valasztott-szak')['egyetem'];
         switch($university) {
@@ -158,7 +238,7 @@ class PointCalculationService
      * @param Request $request
      * @return void
      */
-    private function setResultToCollection(Request $request)
+    private function setResultToCollection(Request $request): void
     {
         $results = $request->get('erettsegi-eredmenyek');
         foreach ($results as $result) {
@@ -168,6 +248,23 @@ class PointCalculationService
                 'result' => str_replace('%', '', $result['eredmeny']),
                 'type'   => $result['tipus']
             ]);
+        }
+    }
+
+    /**
+     * @param Request $request
+     * @return void
+     */
+    private function setExtraToCollection(Request $request): void
+    {
+        $extras = $request->get('tobbletpontok');
+        foreach ($extras as $extra) {
+            if ( $extra['kategoria'] == 'Nyelvvizsga') {
+                $this->extras->push([
+                    'language' => $extra['nyelv'],
+                    'level'    => $extra['tipus'],
+                ]);
+            }
         }
     }
 }
